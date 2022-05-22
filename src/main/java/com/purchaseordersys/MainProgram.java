804674bf5e02d2a8a6720de2853f8307845c6803
nickaldred <nickaldred@hotmail.co.uk>
2022-05-22 09:14:06 +0100

    Took out test code

--- src/main/java/com/purchaseordersys/MainProgram.java
+++ src/main/java/com/purchaseordersys/MainProgram.java
@@ -1,75 +1,10 @@
 package com.purchaseordersys;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
 
 
 
 public class MainProgram {
     public static void main(String[] args) throws Exception{
 
-        // Product car = new Product("ST180", "Fiesta", "Ford", 6000.65, 10000.65, 20, 30);
-        // car.printProduct();
-
-           /*
-        Maven dependency for JSON-simple:
-            <dependency>
-                <groupId>com.googlecode.json-simple</groupId>
-                <artifactId>json-simple</artifactId>
-                <version>1.1.1</version>
-            </dependency>
-         */
-
-        try {
-            //Public API:
-            //https://www.metaweather.com/api/location/search/?query=<CITY>
-            //https://www.metaweather.com/api/location/44418/
-
-            URL url = new URL("http://127.0.0.1:5000/products");
-
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            //Check if connect is made
-            int responseCode = conn.getResponseCode();
-
-            // 200 OK
-            if (responseCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + responseCode);
-            } else {
-
-                StringBuilder informationString = new StringBuilder();
-                Scanner scanner = new Scanner(url.openStream());
-
-                while (scanner.hasNext()) {
-                    informationString.append(scanner.nextLine());
-                }
-                //Close the scanner
-                scanner.close();
-
-                System.out.println(informationString);
-
-
-                //JSON simple library Setup with Maven is used to convert strings to JSON
-                JSONParser parse = new JSONParser();
-                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
-
-                //Get the first JSON object in the JSON array
-                System.out.println(dataObject.get(0));
-
-                JSONObject countryData = (JSONObject) dataObject.get(0);
-
-                System.out.println(countryData.get("woeid"));
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+}
 }
