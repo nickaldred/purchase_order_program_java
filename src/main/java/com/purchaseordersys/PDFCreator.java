031bbd5e06c117a3c1b10d020cd121328a160d39
Nick Aldred <nickaldred@hotmail.co.uk>
2022-06-22 18:51:40 +0100

    Created PDF creator class and started code

--- src/main/java/com/purchaseordersys/PDFCreator.java
+++ src/main/java/com/purchaseordersys/PDFCreator.java
@@ -1,5 +1,30 @@
 package com.purchaseordersys;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 public class PDFCreator {
+
+    public static void main(String[] args)throws FileNotFoundException{
+
+        try{
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("C:/Users/NickA/Documents/Java/PO's/purchaseorder.pdf"));
+        document.open();
+        document.add(new Paragraph("Example"));
+        document.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        System.out.println("Itext executed");
+
+    }
+
+
     
 }
