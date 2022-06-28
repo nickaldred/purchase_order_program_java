de27cdf95c50debe41722f6d9e0f36063951de71
Nick Aldred <nickaldred@hotmail.co.uk>
2022-06-28 19:59:06 +0100

    made adjustments to tester code

--- src/main/java/com/purchaseordersys/MainProgram.java
+++ src/main/java/com/purchaseordersys/MainProgram.java
@@ -6,5 +6,11 @@ package com.purchaseordersys;
 public class MainProgram {
     public static void main(String[] args) throws Exception{
 
+        Product newProduct = new Product("sn8300", "Pull switch 2 way", "MLA", 1.25, 3.95, 5, 20);
+
+        newProduct.printProduct();
+
+        
+        System.out.println(newProduct.productJSON());
 }
 }
