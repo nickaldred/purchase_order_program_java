c60367c84d8341f7349ec88a51fa6f710fbf82da
Nick Aldred <nickaldred@hotmail.co.uk>
2022-06-28 19:55:26 +0100

    Write image and heading to PDF

--- src/main/java/com/purchaseordersys/PDFCreator.java
+++ src/main/java/com/purchaseordersys/PDFCreator.java
@@ -2,26 +2,54 @@ package com.purchaseordersys;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import com.itextpdf.text.Document;
+import com.itextpdf.io.image.ImageData; 
+import com.itextpdf.io.image.ImageDataFactory; 
 
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.PdfDocument; 
+import com.itextpdf.kernel.pdf.PdfWriter;
+
+import com.itextpdf.layout.Document; 
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;  
+
 
 public class PDFCreator {
 
     public static void main(String[] args)throws FileNotFoundException{
 
         try{
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream("C:/Users/NickA/Documents/Java/PO's/purchaseorder.pdf"));
-        document.open();
-        document.add(new Paragraph("Example"));
-        document.close();
-        }catch(Exception e){
-            System.out.println(e);
-        }
+            // Creating a PdfWriter       
+            String dest = "C:/Users/NickA/Documents/Java/PO's/purchaseorder.pdf";       
+            PdfWriter writer = new PdfWriter(dest);        
+            
+            // Creating a PdfDocument       
+            PdfDocument pdf = new PdfDocument(writer);              
+            
+            // Creating a Document        
+            Document document = new Document(pdf);              
+            
+            // Creating an ImageData object       
+            String imFile = "C:/Users/NickA/Documents/Java/Projects - 2/purchase_order_sys/src/files/header.png";       
+            ImageData data = ImageDataFactory.create(imFile);              
+            
+            //Add header image to PDF    
+            Image image = new Image(data);                          
+            document.add(image);
+            
+            //Add heading to document
+            String heading = "Purchase Order";
+            Paragraph para = new Paragraph(heading);
+            document.add(para);
+            
+            //Close document      
+            document.close();              
 
-        System.out.println("Itext executed");
+            
+            }catch(Exception e){
+                System.out.println(e);
+            }
+
+            System.out.println("Itext executed");
 
     }
 
